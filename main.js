01fb18c2806a6a8f75d1150928e51af2e81f5b99
⚡ ⚡ <galbujar@mail.valenciacollege.edu>
2022-09-12 12:59:32 -0400

    finished

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstName;
+firstName = "Gilmaris";
+let lastName;
+lastName = " Albujar";
+let age;
+age = 24;
+alert("My name is " + firstName + lastName);
+alert("I am " + age + " years old");
+console.log("I am having so much fun in class");
